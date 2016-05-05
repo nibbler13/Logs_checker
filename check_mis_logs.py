24c3ac0b43b12b5f8310e97def3f95eb1ddd7ab9
nibbler13 <nibble@yandex.ru>
2016-05-06 02:55:09 +0300

    Update check_mis_logs.py

--- check_mis_logs.py
+++ check_mis_logs.py
@@ -2,7 +2,7 @@
 Check logs file and send email with results
 Settings must be in the file check_mis_logs_config.ini at script directory
 
-Author: nibbler13 nn-admin@nnkk.budzdorov.su 31-555
+Author: nibbler13 nn-_____@_____._____.su 31-555
 v1.0
 2016/04/21
 Python version 3.5.1
